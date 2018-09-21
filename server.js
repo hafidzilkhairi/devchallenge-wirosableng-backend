9b017f9799661e4b25fccc0cd7e20c491559594b
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2018-09-21 11:05:57 +0700

    mengubah nomor port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ expressSession({secret: 'max',saveUninitialized: false, resave: false})
 // Config the host and port
 const server = new Hapi.Server({
     host: 'localhost',
-    port: '3000'
+    port: '3001'
 })
 
 async function liftOff() {
